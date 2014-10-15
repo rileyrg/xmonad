577ec0c37fb8b9b23862afaa189ae2a115463912
Richard Riley <rileyrg@gmail.com>
2014-10-15 12:32:28 +0200

    comment

--- xmonad.hs
+++ xmonad.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE ScopedTypeVariables #-}
-
 -- XMonad setup for debian
+-- Richard G. Riley
 -- rileyrg@gmail.com
 
 import           XMonad
@@ -28,7 +28,10 @@ import           XMonad.Hooks.SetWMName
 import           XMonad.Layout.Circle
 import           XMonad.Layout.DragPane
 import           XMonad.Layout.Grid
+import           XMonad.Layout.Fullscreen
 import           XMonad.Layout.MultiToggle
+import           XMonad.Layout.NoBorders
+import           XMonad.Layout.PerWorkspace 
 import           XMonad.Layout.MultiToggle.Instances
 import           XMonad.Layout.Reflect
 import           XMonad.Prompt
@@ -42,7 +45,7 @@ import           XMonad.Util.SpawnOnce
 -- import XMonad.Hooks.DebugStack
 
 myWorkSpaces :: [String]
-myWorkSpaces = ["1:Desktop", "2:TV" ,"3:IDE","4:Wireshark"]
+myWorkSpaces = ["1:General", "2:General" ,"3:IDE","4:Wireshark","5:TV"]
 
 myManageHook :: ManageHook
 myManageHook =  composeAll . concat $
@@ -50,8 +53,10 @@ myManageHook =  composeAll . concat $
                                [fmap(i `isPrefixOf`) resource --> doIgnore  | i <- myIgnores]
                                ,[fmap(f `isPrefixOf`) resource --> doCenterFloat   | f <- myFloats]
                                ,[
-                                 className =? "jetbrains-idea-ce"  --> doShift "3:IDE"
-                               , className =? "Eclipse"  --> doShift "3:IDE"
+                                 className =? "SpiderOak"  --> doShift "2:General"
+                               , className =? "Android SDK Manager"  --> doShift "2:General"
+                               , className =? "jetbrains-idea-ce"  --> doShift "3:IDE"
+                               , className =? "Eclipse"  --> doShift "3:IDE" 
                                , className =? "Wireshark"  --> doShift "4:Wireshark"
                                ,isDialog  --> doCenterFloat
                                 ]
@@ -62,30 +67,35 @@ myManageHook =  composeAll . concat $
       myIgnores       = ["xx","xxx"]
 
 myLayout =
-    -- onWorkspace "2:TV"   (noBorders (fullscreenFull  Full )) $
-    -- onWorkspace "3:IDE"  (noBorders (fullscreenFull  Full )) $
-    -- onWorkspace "4:Wireshark"   (noBorders (fullscreenFull  Full )) $
+    onWorkspace "1:General" (avoidStruts $ (mkToggle(FULL??EOT) (Tall 1 (3/100) (1/2))))  $
+    onWorkspace "3:IDE"  (noBorders (fullscreenFull  Full )) $
+    onWorkspace "4:Wireshark"   (noBorders (fullscreenFull  Full )) $
+    onWorkspace "5:TV"   (noBorders (fullscreenFull  Full )) $
     avoidStruts $ mkToggle (single REFLECTY) $ mkToggle (single REFLECTX) $ mkToggle (single MIRROR) $ mkToggle(FULL??EOT) (Tall 1 (3/100) (1/2) ||| dragPane Horizontal 0.1 0.5||| Grid ||| Circle )
 
 myTerminal :: String
 myTerminal = "urxvt"
 scratchpads :: [NamedScratchpad]
 scratchpads = [
-
-     NS "9patchresizer" "9patchresizer" (className =?"9Patch Resizer") nonFloating,
+  
+     NS "9patchresizer" "9patchresizer.sh" (className =?"9Patch Resizer") nonFloating,
      NS "androidemulator" "emulator -avd default" (className =?"emulator64-arm") nonFloating,
+     NS "groovyconsole" "groovyConsole" (className =? "org-codehaus-groovy-tools-GroovyStarter") nonFloating,
      NS "chrome" "google-chrome --new-window" (className =? "Google-chrome") nonFloating ,
-     NS "conky" "conky -c ${HOME}/.conky/conkyrc -q" (className =? "Conky") doFloat ,
+     NS "conky" "conky -c ${HOME}/.conkyrc -q" (className =? "Conky") doFloat ,
      NS "emacs" "edit -c" (className =? "Emacs") nonFloating,
      NS "evince" "evince" (className =? "evince") nonFloating ,
      NS "gimp" "gimp" (className =? "Gimp") nonFloating ,
-     NS "firefox" "firefox" (className =? "Firefox") nonFloating ,
+     NS "firefox" "firefox" (className =? "Iceweasel") nonFloating ,
+     NS "torbrowser" "torbrowser-launcher" (className =? "Tor Browser") nonFloating ,
      NS "htop" "urxvt -name htop -title htop -e htop" (title =? "htop") nonFloating,
-     NS "intellij" "intellij" (className =?"jetbrains-idea-ce") nonFloating,
-     NS "pcmanfm" "pcmanfm" (appName =?"pcmanfm") nonFloating,
+     NS "intellij" "idea.sh" (className =?"jetbrains-idea-ce") nonFloating,
+     NS "filemanager" "pcmanfm" (className =?"Pcmanfm") nonFloating,
      NS "rhythmbox" "rhythmbox" (className =? "Rhythmbox") nonFloating ,
      NS "terminal" "mytoggleterm" (appName =? "terminal") nonFloating,
      NS "wireshark" "wireshark" (className =? "Wireshark") nonFloating ,
+     NS "pavucontrol" "pavucontrol" (title =? "Volume Control") nonFloating ,
+     NS "acroread" "acroread" (className =? "Acroread-en") nonFloating ,
      NS "xfce4-appfinder" "xfce4-appfinder" (className =? "Xfce4-appfinder") nonFloating
  ]
 
@@ -107,12 +117,15 @@ myKeys= [
               ("<Print>", spawn "fullscreenshot")
              ,("M-s", SM.submap $ smap $ S.promptSearchBrowser greenXPConfig "firefox")
              ,("M-g",  S.promptSearchBrowser greenXPConfig "firefox" S.google)
-             ,("M-<F1>", AL.launchApp def "gnome-terminal -x info " )
-             ,("M-S-2",namedScratchpadAction scratchpads "9patchresizer")
+             ,("M-<F1>", AL.launchApp def "urxvt -e info " )
+             ,("M-S-r",namedScratchpadAction scratchpads "9patchresizer")
+             ,("M-S-a",namedScratchpadAction scratchpads "acroread")
              ,("M-S-c",namedScratchpadAction scratchpads "chrome")
              ,("M-S-d",namedScratchpadAction scratchpads "evince")
+             ,("M-S-y",namedScratchpadAction scratchpads "groovyconsole")
              ,("M-S-e",namedScratchpadAction scratchpads "emacs")
              ,("M-S-f",namedScratchpadAction scratchpads "firefox")
+             ,("M-S-t",namedScratchpadAction scratchpads "torbrowser")
              ,("M-S-g",namedScratchpadAction scratchpads "gimp")
              ,("M-S-i",namedScratchpadAction scratchpads "intellij")
              ,("M-S-w",namedScratchpadAction scratchpads "wireshark")
@@ -122,11 +135,12 @@ myKeys= [
              ,("M-C-a",namedScratchpadAction scratchpads "xfce4-appfinder")
              ,("M-C-c",namedScratchpadAction scratchpads "conky")
              ,("M-C-h",sshPrompt def)
-             ,("M-C-n",namedScratchpadAction scratchpads "pcmanfm")
+             ,("M-C-f",namedScratchpadAction scratchpads "filemanager")
              ,("M-C-p",namedScratchpadAction scratchpads "htop")
              ,("M-C-s",spawn "screenshot")
              ,("M-C-t",namedScratchpadAction scratchpads "terminal")
              ,("M-C-x",spawn "xinfoatmouse")
+             ,("M-C-v",spawn "pavucontrol")
 
              -- workspace management
 
@@ -142,7 +156,6 @@ myKeys= [
              ,("M-k",  rotSlavesUp)
              ,("M-u",  rotFocusedDown)
              ,("M-i",  rotFocusedUp)
-             ,("M-C-z",     toggleWS)
              ,("M-x", sendMessage $ Toggle MIRROR)
              ,("M-f", sendMessage $ Toggle FULL)
              ,("M-y", sendMessage $ Toggle REFLECTX)
@@ -150,26 +163,30 @@ myKeys= [
 
              -- session management
 
-             ,("M-C-f", withFocused float)
+             -- ,("M-C-f", withFocused float)
              ,("M-C-<Delete>", spawn "xkill")
              ,("M-S-<Delete>", kill)
              ,("M-C-l", spawn "xscreensaver-command -l")
              -- Quit xmonad
+             ,("M-C-z", spawn "confirmtoquit --suspend")
              ,("M-C-d", spawn "confirmtoquit --shutdown")
              ,("M-C-q", spawn "confirmtoquit")
              ,("M-C-r", spawn "confirmtoquit --restart")
-             ,("M-C-v", spawn "gnome-volume-control")
              ]
 
 myStartUpHook :: X ()
 myStartUpHook  =   do
   setWMName "LG3D" -- Java progs need this a jdk 1.6+
   spawnOnce "xcompmgr"
-  spawnOnce "feh --bg-fill ${WALLPAPER}"
-  spawnOnce "dropbox start -i"
   spawnOnce "xscreensaver -no-splash"
-  spawnOnce "sleep 1 && wicd-client --tray"
-  spawnOnce "sleep 1 &&  stalonetray"
+  spawnOnce "stalonetray"
+  spawnOnce "feh --bg-fill ${WALLPAPER}"
+  spawnOnce "sleep 2 && xfce4-power-manager" 
+  spawnOnce "sleep 2 && SpiderOak"
+  spawnOnce "sleep 2 && dropbox start -i"
+  spawnOnce "sleep 2 && wicd-client --tray"
+
+
 
 myFadeHook = composeAll [
               isUnfocused --> transparency 0.2
@@ -178,10 +195,10 @@ myFadeHook = composeAll [
 
 myConfig p =  def {
              manageHook = myManageHook <+> namedScratchpadManageHook scratchpads <+> manageDocks
-           ,logHook = fadeWindowsLogHook myFadeHook <+> updatePointer (0.5, 0.5) (0, 0) <+> dynamicLogWithPP xmobarPP {
+--           ,logHook = fadeWindowsLogHook myFadeHook <+> updatePointer (0.5, 0.5) (0, 0) <+> dynamicLogWithPP xmobarPP {
+           ,logHook = fadeWindowsLogHook myFadeHook <+> dynamicLogWithPP xmobarPP {             
 --           ,logHook = fadeWindowsLogHook myFadeHook <+>  updatePointer (Relative 0.5 0.5)  <+> dynamicLogWithPP xmobarPP {
              ppOutput = hPutStrLn p
-             ,ppOrder               = \(ws:l:t:_)   -> [ws]
              }  <+> ewmhDesktopsLogHook -- <+> debugStackLogHook
 -- <+> debugStackLogHook
            ,handleEventHook = fadeWindowsEventHook
